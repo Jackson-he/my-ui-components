707b1a1aacd3c793e0c678126bbbd3532667737e
何绪杰 <hexujie0824@gmail.com>
2019-01-02 14:08:14 +0800

    表单输入值对比组件

---
+++ inputChangeCompare.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  data () {
+    return {
+      val: '', // 组件内维护的输入框的值
+      dataChanged: false, // 数据是否有更改
+      tracked: false, // 数据是否已追踪
+      originVal: '', // 原始值
+      typeofValue: '', // 输入值的类型
+      DASHED: ''
+    }
+  },
+  props: {
+    value: {
+      type: [ String, Number ]
+    }
+  },
+  watch: {
+    'value': {
+      handler (newVal) {
+        this.val = newVal
+        this.typeofValue = typeof newVal
+
+        !this.tracked && (this.originVal = newVal)
+
+        this.dataChanged = +newVal !== +this.originVal
+
+        this.tracked = true
+      },
+      immediate: true
+    }
+  },
+  render () {
+    const props = {
+      attrs: { ...this.$attrs }
+    }
+
+    return <el-input {...props} value={ this.val } onInput={ this.OnInput } nativeOn-keydown={ this.OnKeydown } class={ this.dataChanged ? 'red-for-change-record' : ''}></el-input>
+  },
+  methods: {
+    OnInput (val) {
+      this.val = this.typeofValue === 'number' ? +val : val
+      this.$emit('input', this.val)
+    },
+    OnKeydown (event) {
+      const reg = /([0-9]*\.?)(.*)/
+      this.DASHED = this.val.match(reg)[2]
+
+      const deleteKeyCode = 46
+      const backSpaceKeyCode = 8
+
+      if (this.DASHED.length > 1 && ![deleteKeyCode, backSpaceKeyCode].includes(event.keyCode)) {
+        event.returnValue = false
+      }
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .red-for-change-record {
+    input {
+      color: red!important;
+    }
+  }
+</style>
