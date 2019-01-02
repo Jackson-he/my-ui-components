50161fd2633332b573b8805ca1a938dce9deb2a2
何绪杰 <hexujie0824@gmail.com>
2019-01-02 16:50:59 +0800

    输入框限制完善

--- inputChangeCompare.vue
+++ inputChangeCompare.vue
@@ -37,20 +37,28 @@ export default {
 
     return <el-input {...props} value={ this.val } onInput={ this.OnInput } nativeOn-keydown={ this.OnKeydown } class={ this.dataChanged ? 'red-for-change-record' : ''}></el-input>
   },
+  created () {
+    const reg = /([0-9]*\.?)(.*)/
+    this.DASHED = String(this.val).match(reg)[2]
+  },
   methods: {
     OnInput (val) {
+      const reg = /([0-9]*\.?)(.*)/
+      this.DASHED = val.match(reg)[2]
+
       this.val = this.typeofValue === 'number' ? +val : val
       this.$emit('input', this.val)
     },
     OnKeydown (event) {
-      const reg = /([0-9]*\.?)(.*)/
-      this.DASHED = this.val.match(reg)[2]
-
       const deleteKeyCode = 46
       const backSpaceKeyCode = 8
+      const isEmptyKey = [deleteKeyCode, backSpaceKeyCode].includes(event.keyCode)
 
-      if (this.DASHED.length > 1 && ![deleteKeyCode, backSpaceKeyCode].includes(event.keyCode)) {
-        event.returnValue = false
+      if (this.DASHED.length > 0 && !isEmptyKey) {
+        event.returnValue = /^\d*$/.test(event.key)
+        if (this.DASHED.length > 1) {
+          event.returnValue = false
+        }
       }
     }
   }
